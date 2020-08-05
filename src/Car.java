a9ba74e0daa4b9233e72ea32ad79b2dd293459ce
sish11 <69053365+sish11@users.noreply.github.com>
2020-08-05 10:01:51 +0200

    Add files via upload

---
+++ src/Car.java
@@ -0,0 +1,46 @@
+public class Car implements Vehicle {
+
+    //instance variables
+    String numberPlate;
+    String color;
+    String model;
+    String type;
+    double base_rate;
+
+    //constructor
+    Car(String numberPlate, String color, String model, String type) {
+        this.numberPlate = numberPlate;
+        this.color = color;
+        this.model = model;
+        this.type = type;
+        if(type.equals("XL")) {
+            this.base_rate = 15.00;
+        } else {
+            this.base_rate = 10.00;
+        }
+    }
+
+    //methods
+    public String getNumberPlate() {
+        return this.numberPlate;
+    }
+
+    public double getBaseRate() {
+        return this.base_rate;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getVehicleType() {
+        return this.type;
+    }
+
+    public String toString() {
+        return this.model + " car of " + this.color + " color, with number plate: " + this.numberPlate;
+    }
+}
